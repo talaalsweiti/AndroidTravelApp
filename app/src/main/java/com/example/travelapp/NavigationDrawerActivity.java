52d0817251d01d16b0c8367b41a0adf007e57b3c
talaalsweiti <talaalsweiti@gmail.com>
2023-01-28 22:20:06 +0300

    remove the messaging  icon

--- app/src/main/java/com/example/travelapp/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/travelapp/NavigationDrawerActivity.java
@@ -38,13 +38,7 @@ public class NavigationDrawerActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
-        binding.appBarNavigationDrawer.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
