96794130d62e8950a869d6002eec82c9a4058a7a
yasmin <yasmin200062@hotmail.com>
2023-02-10 21:31:31 +0200

    refresh favorites fragment

--- app/src/main/java/com/example/travelapp/DestinationActivity.java
+++ app/src/main/java/com/example/travelapp/DestinationActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,9 +32,9 @@ public class DestinationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
-        Button description = findViewById(R.id.description);
-        Button image = findViewById(R.id.image);
-        Button location = findViewById(R.id.location);
+        View description = findViewById(R.id.description);
+        View image = findViewById(R.id.image);
+        View location = findViewById(R.id.location);
         TextView cityText = findViewById(R.id.city);
         cityText.setText(city);
         currentUser = NavigationDrawerActivity.user;
